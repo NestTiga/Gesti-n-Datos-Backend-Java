fcb3cb376d33add6bdcaeddb3dfce95aaf4212f1
NestTiga <nestortigasi371@gmail.com>
2023-11-15 14:33:34 -0500

    Mejorando el controller

--- src/main/java/com/tgdev/sistemaweb/controllers/PersonaController.java
+++ src/main/java/com/tgdev/sistemaweb/controllers/PersonaController.java
@@ -1,6 +1,22 @@
 package com.tgdev.sistemaweb.controllers;
 
+import com.tgdev.sistemaweb.models.Persona;
+import com.tgdev.sistemaweb.services.PersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequestMapping("api/personas")
+@RestController
 public class PersonaController {
+    @Autowired
+    private PersonaService personaService;
 
+    @GetMapping
+    public List<Persona> listarPersona(){
+        return personaService.obtenerPersonas();
+    }
 }
